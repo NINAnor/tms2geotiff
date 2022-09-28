3c9f1550db72681f3d88816b174da028df9a038a
Francesco Frassinelli <francesco.frassinelli@nina.no>
2022-09-28 15:14:38 +0200

    Save to a different file

--- geotiff2grayscale.py
+++ geotiff2grayscale.py
@@ -5,12 +5,15 @@ from osgeo import gdal
 import argparse
 
 
-def grayscale(path):
+def grayscale(input_path, output_path):
+    src = gdal.Open(input_path, gdal.gdalconst.GA_Update)
+    driver = gdal.GetDriverByName('GTiff')
+    dst = driver.CreateCopy(output_path, src, 1)
+
     colors = gdal.ColorTable()
     colors.CreateColorRamp(0, (0, 0, 0), 255, (255, 255, 255))
 
-    src = gdal.Open(path, gdal.gdalconst.GA_Update)
-    band = src.GetRasterBand(1)
+    band = dst.GetRasterBand(1)
     band.SetRasterColorTable(colors)
     band.SetRasterColorInterpretation(gdal.GCI_PaletteIndex)
     del src
@@ -18,9 +21,10 @@ def grayscale(path):
 
 def main():
     parser = argparse.ArgumentParser(description="Convert GeoTIFFs with palette to grayscale")
-    parser.add_argument("path")
+    parser.add_argument("input")
+    parser.add_argument("output")
     args = parser.parse_args()
-    grayscale(args.path)
+    grayscale(args.input, args.output)
 
 
 if __name__ == "__main__":
